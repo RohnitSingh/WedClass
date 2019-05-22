cf14a842ada07fd3e75f539b61eabd72e28f4b0e
RohnitSingh <singrohn@sheridancollege.ca>
2019-05-22 14:11:13 -0400

    This is student demo programme

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -14,8 +14,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  */
 public class StudentDemo {
 
-    private String nameR;
-    private int studentId;
+    private String name;
 
     //constructor
     public StudentDemo(String givenName)
